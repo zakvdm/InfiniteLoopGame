94cb2f6fd66feb16ae2190782f81f4a9f9da80d8
Zak van der Merwe <zakvdm@gmail.com>
2012-07-17 10:42:54 +0200

    Some more preliminary work.

--- client/actor/gamescene.js
+++ client/actor/gamescene.js
@@ -90,12 +90,12 @@
 
 (function() {
     FNT.LevelContainer = function() {
-        FNT.LevelActor.superclass.constructor.call(this);
+        FNT.LevelContainer.superclass.constructor.call(this);
         this.ringActors = [];
         return this;
     };
 
-    FNT.LevelActor.prototype= {
+    FNT.LevelContainer.prototype= {
         ringActors:    null,
 
         initialize : function() {
@@ -133,7 +133,7 @@
         }
     };
 
-    extend(FNT.LevelActor, CAAT.ActorContainer);
+    extend(FNT.LevelContainer, CAAT.ActorContainer);
 })();
 
 (function() {
@@ -148,8 +148,6 @@
         directorScene:              null,
 
         levelContainer:             null,
-
-        timer:                      null,
 
         director:                   null,
 
@@ -177,7 +175,7 @@
 
             this.directorScene = director.createScene();
             this.directorScene.activated = function() {
-                me.prepareSound();
+                //me.prepareSound();
             };
 
             var dw = director.width;
@@ -187,8 +185,6 @@
                     create().
                     setSize(dw, dh).
                     setLocation(0, 0);
-
-            this.directorScene.addChild(this.ringsContainer);
 
             this.levelContainer.loadLevel(this.context.getLevel());
 
@@ -233,190 +229,7 @@
                 this.gameListener[i].gameEvent(type, data);
             }
         },
-        setDifficulty : function(level) {
-            this.context.difficulty=level;
-        },
-        cancelTimer : function(){
-            if ( this.timer!=null ) {
-                this.timer.cancel();
-            }
-            this.timer= null;
-        },
-        enableTimer : function() {
-            var me= this;
-            
-            this.timer= this.directorScene.createTimer(
-                this.directorScene.time,
-                this.context.turnTime,
-                function timeout(sceneTime, time, timerTask) {
-                    me.context.timeUp();
-                },
-                function tick(sceneTime, time, timerTask) {
-                    me.chronoActor.tick(time, timerTask.duration);
-                });
-
-        },
-        setGameMode : function(gameMode) {
-            this.context.setGameMode(gameMode);
-        },
-        endLevel : function() {
-            var level= this.context.level;
-            if ( level>7 ) {
-                level=7;
-            }
-            var image= this.director.getImage( 'msg'+level);
-            if ( null!=image ) {
-                this.endLevelMessage.setBackgroundImage( image, true );
-                this.endLevelMessage.setLocation(
-                        (this.endLevelMessage.parent.width-image.width)/2,
-                        this.endLevelMessage.parent.height/2 - 25
-                        );
-            }
-            this.showGameEvent( this.endLevelActor );
-        },
-        removeGameEvent : function( actor, callback ) {
-            actor.enableEvents(false);
-            this.uninitializeActors();
-
-            var me= this;
-
-            actor.emptyBehaviorList();
-            actor.addBehavior(
-                new CAAT.PathBehavior().
-                    setFrameTime( actor.time, 2000 ).
-                    setPath(
-                        new CAAT.LinearPath().
-                                setInitialPosition( actor.x, actor.y ).
-                                setFinalPosition( actor.x, -actor.height )
-                    ).
-                    setInterpolator(
-                        new CAAT.Interpolator().createExponentialInInterpolator(2,false)
-                    ).
-                    addListener(
-                        {
-                            behaviorExpired : function(behavior, time, actor) {
-                                actor.setOutOfFrameTime();
-                                callback();
-                            }
-                        }
-                    )
-            );
-        },
-        showGameEvent : function(actor) {
-            // parar y eliminar cronometro.
-            this.cancelTimer();
-
-            // quitar contorl de mouse.
-            this.bricksContainer.enableEvents(false);
-
-            // mostrar endgameactor.
-
-            var x= (this.directorScene.width - actor.width)/2;
-            var y= (this.directorScene.height - actor.height)/2 - 100;
-
-            var me_endGameActor= actor;
-            actor.emptyBehaviorList().
-                setFrameTime(this.directorScene.time, Number.MAX_VALUE).
-                enableEvents(false).
-                addBehavior(
-                    new CAAT.PathBehavior().
-                        setFrameTime( this.directorScene.time, 2000 ).
-                        setPath(
-                            new CAAT.LinearPath().
-                                setInitialPosition( x, this.directorScene.height ).
-                                setFinalPosition( x, y ) ).
-                        setInterpolator(
-                            new CAAT.Interpolator().createExponentialInOutInterpolator(3,false) ).
-                        addListener( {
-                            behaviorExpired : function(behavior, time, actor) {
-                                me_endGameActor.enableEvents(true);
-
-                                me_endGameActor.emptyBehaviorList();
-                                me_endGameActor.addBehavior(
-                                        new CAAT.PathBehavior().
-                                            setFrameTime( time, 3000 ).
-                                            setPath(
-                                                new CAAT.LinearPath().
-                                                        setInitialPosition( me_endGameActor.x, me_endGameActor.y ).
-                                                        setFinalPosition(
-                                                            me_endGameActor.x+(Math.random()<.5?1:-1)*(5+5*Math.random()),
-                                                            me_endGameActor.y+(Math.random()<.5?1:-1)*(5+5*Math.random()) )
-                                            ).
-                                            addListener( {
-                                                behaviorExpired : function(behavior, time, actor) {
-                                                    behavior.setFrameTime( time, 3000 );
-                                                    behavior.path.setFinalPosition(
-                                                            me_endGameActor.x+(Math.random()<.5?1:-1)*(5+5*Math.random()),
-                                                            me_endGameActor.y+(Math.random()<.5?1:-1)*(5+5*Math.random())
-                                                            );
-                                                },
-                                                behaviorApplied : function(behavior, time, normalizedTime, actor, value) {
-                                                }
-                                            }).
-                                            setInterpolator(
-                                                new CAAT.Interpolator().createExponentialInOutInterpolator(3,true)
-                                                )
-                                        );
-
-                            },
-                            behaviorApplied : function(behavior, time, normalizedTime, actor, value) {
-                            }
-                        } )
-                );
-        },
-        soundControls : function(director) {
-            var ci= new CAAT.SpriteImage().initialize( director.getImage('sound'), 2,3 );
-            var dw= director.width;
-            var dh= director.height;
-
-            var music= new CAAT.Actor().
-                    setAsButton( ci.getRef(),0,1,0,0, function(button) {
-                        director.audioManager.setMusicEnabled( !director.audioManager.isMusicEnabled() );
-                        if ( director.audioManager.isMusicEnabled() ) {
-                            button.setButtonImageIndex(0,1,0,0);
-                        } else {
-                            button.setButtonImageIndex(2,2,2,2);
-                        }
-                    }).
-                    setBounds( dw-ci.singleWidth-2, 2, ci.singleWidth, ci.singleHeight );
-
-            var sound= new CAAT.Actor().
-                    setAsButton( ci.getRef(),3,4,3,3, function(button) {
-                        director.audioManager.setSoundEffectsEnabled( !director.audioManager.isSoundEffectsEnabled() );
-                        if ( director.audioManager.isSoundEffectsEnabled() ) {
-                            button.setButtonImageIndex(3,4,3,3);
-                        } else {
-                            button.setButtonImageIndex(5,5,5,5);
-                        }
-                    });
-            if ( director.width>director.height ) {
-                sound.setBounds( dw-ci.singleWidth-2, 2+2+ci.singleHeight, ci.singleWidth, ci.singleHeight );
-            } else {
-                sound.setBounds( dw-ci.singleWidth*2-2, 2+2, ci.singleWidth, ci.singleHeight );
-            }
-
-            music.prepare= function() {
-                if ( director.audioManager.isMusicEnabled() ) {
-                    this.setButtonImageIndex(0,1,0,0);
-                } else {
-                    this.setButtonImageIndex(2,2,2,2);
-                }
-            }
-
-            sound.prepare= function() {
-                if ( director.audioManager.isSoundEffectsEnabled() ) {
-                    this.setButtonImageIndex(3,4,3,3);
-                } else {
-                    this.setButtonImageIndex(5,5,5,5);
-                }
-            }
-
-            this.directorScene.addChild(sound);
-            this.directorScene.addChild(music);
-
-            this.music= music;
-            this.sound= sound;
-        },
+     
         prepareSound : function() {
             try {
                 this.sound.prepare();
